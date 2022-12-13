4fb5f58447571055b83856392bc24b272d9c774a
Luka Kikelj <kikelj.luka@gmail.com>
2022-12-13 11:41:24 +0100

    basic files configuration App.vue, main.js & Navbar.vue

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <header>
+    <navbar v-for="item in items" :name="item.name" :key="item.id"></navbar>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          id: "products",
+          name: "Products",
+        },
+        {
+          id: "resources",
+          name: "Resources",
+        },
+        {
+          id: "partners",
+          name: "Partners",
+        },
+        {
+          id: "company",
+          name: "Company",
+        },
+        {
+          name: "Login",
+        },
+        {
+          name: "En",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar">
+    <ul class="navbar__list">
+      <li class="navbar__item">
+        <a class="navbar__link" href="">{{ name }}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+    },
+  },
+  //   data() {},
+};
+</script>
+
+<style></style>
